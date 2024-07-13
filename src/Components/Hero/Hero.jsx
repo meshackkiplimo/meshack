e7f1e68bab3d07db6cd3371c5c2a0302a7d8406d
meshack <meshackkimaiyo5@gmail.com>
2024-07-13 19:57:09 +0300

    portofolio ui first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+
+import './Hero.css'
+import profile_img from '../../assets/profile_img.svg'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+    <img src={profile_img} className='img' />
+    <h1>Iam Meshack Kimaiyo , A software Engineer Based in Nairobi, Kenya </h1>
+      
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+
+import './Navbar.css'
+import logo from '../../assets/logo.svg'
+
+const Navbar = () => {
+  return (
+    <div className= 'navbar'>
+      <img src={logo}/>
+      <ul className='nav-menu'>
+      <li>Home</li>
+      <li>About me</li>
+      <li>Services</li>
+      <li>Portofolio</li>
+      <li>Contact</li>
+
+      </ul>
+      <div className='nav-connect'>
+      Connect with me
+
+      </div>
+
+      
+    </div>
+  )
+}
+
+export default Navbar
