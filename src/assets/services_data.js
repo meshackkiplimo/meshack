cfa3eff18a309a9988d1a2b42e364f9111868269
meshack <meshackkimaiyo5@gmail.com>
2024-08-14 19:33:49 +0300

    final changes

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,34 +1,46 @@
 const Services_Data = [
-    {
-        s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
-    },
-]
- 
+  {
+    s_no: "01",
+    s_name: "Web design",
+    s_desc:
+      "Web design involves creating the visual layout, user interface, and overall aesthetic of a website.",
+  },
+  {
+    s_no: "02",
+    s_name: "Graphics design",
+    s_desc:
+      "Graphics design encompasses creating visual content to communicate messages using typography, imagery, and color.",
+  },
+  {
+    s_no: "03",
+    s_name: "Social media",
+    s_desc:
+      "Social media services include managing social media accounts, creating content, and engaging with audiences on platforms like Facebook, Twitter, and Instagram.",
+  },
+  {
+    s_no: "04",
+    s_name: "App design",
+    s_desc:
+      "App design involves creating the user interface and user experience for mobile and web applications, ensuring they are user-friendly and aesthetically pleasing.",
+  },
+  {
+    s_no: "05",
+    s_name: "Digital marketing",
+    s_desc:
+      "Digital marketing includes online strategies such as SEO, email marketing, content marketing, and PPC to promote products or services.",
+  },
+  {
+    s_no: "06",
+    s_name: "Content writing",
+    s_desc:
+      "Content writing focuses on creating engaging, informative, and relevant content for websites, blogs, social media, and other digital platforms.",
+  },
+  {
+    s_no: "07",
+    s_name: "Data Analysis",
+    s_desc:
+      "Data analysis involves inspecting, cleansing, transforming, and modeling data to discover useful information, draw conclusions, and support decision-making.",
+  },
+];
+
 export default Services_Data;
